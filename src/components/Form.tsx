4d5fffaabf73577aedef920aaac02cbc13835bc5
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-10-27 21:04:11 +0530

    Close on Save

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -22,6 +22,7 @@ const Form: FC<FormProps> = ({ formVisibility, onNewNote }) => {
 
   const onSave = () => {
     onNewNote({ id: Date.now().toString(), text: noteText });
+    formVisibility(false);
   };
 
   const onClose = () => {
